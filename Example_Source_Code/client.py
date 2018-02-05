2d3314c63f217bb4f33a8618cdde365ddcd1bb16
MSI-GP62-7RD <light.year0728@gmail.com>
2018-02-05 15:57:41 +0700

    Share a prototype

--- Example_Source_Code/client.py
+++ Example_Source_Code/client.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env python3
 """Script for Tkinter GUI chat client."""
 from socket import AF_INET, socket, SOCK_STREAM
 from threading import Thread
@@ -11,56 +10,51 @@ def receive():
         try:
             msg = client_socket.recv(BUFSIZ).decode("utf8")
             msg_list.insert(tkinter.END, msg)
-        except OSError:  # Possibly client has left the chat.
+        except OSError:
             break
 
 
-def send(event=None):  # event is passed by binders.
+def send(event=None):
     """Handles sending of messages."""
     msg = my_msg.get()
-    my_msg.set("")  # Clears input field.
+    my_msg.set("")
     client_socket.send(bytes(msg, "utf8"))
-    if msg == "{quit}":
+    if msg == "{exit}":
         client_socket.close()
         top.quit()
 
 
 def on_closing(event=None):
     """This function is to be called when the window is closed."""
-    my_msg.set("{quit}")
+    my_msg.set("{exit}")
     send()
 
 top = tkinter.Tk()
 top.title("Chatter")
 
 messages_frame = tkinter.Frame(top)
-my_msg = tkinter.StringVar()  # For the messages to be sent.
-my_msg.set("Type your messages here.")
-scrollbar = tkinter.Scrollbar(messages_frame)  # To navigate through past messages.
-# Following will contain the messages.
+scrollbar = tkinter.Scrollbar(messages_frame)
 msg_list = tkinter.Listbox(messages_frame, height=15, width=50, yscrollcommand=scrollbar.set)
 scrollbar.pack(side=tkinter.RIGHT, fill=tkinter.Y)
 msg_list.pack(side=tkinter.LEFT, fill=tkinter.BOTH)
-msg_list.pack()
 messages_frame.pack()
 
-entry_field = tkinter.Entry(top, textvariable=my_msg)
+action_frame = tkinter.Frame(top)
+my_msg = tkinter.StringVar()
+my_msg.set("")
+entry_field = tkinter.Entry(action_frame, textvariable=my_msg)
 entry_field.bind("<Return>", send)
-entry_field.pack()
-send_button = tkinter.Button(top, text="Send", command=send)
-send_button.pack()
+entry_field.pack(side=tkinter.LEFT, fill=tkinter.X)
+send_button = tkinter.Button(action_frame, text="Send", command=send)
+send_button.pack(side=tkinter.RIGHT)
+action_frame.pack()
 
 top.protocol("WM_DELETE_WINDOW", on_closing)
 
-#----Now comes the sockets part----
-HOST = input('Enter host: ')
-PORT = input('Enter port: ')
-if not PORT:
-    PORT = 33000
-else:
-    PORT = int(PORT)
+HOST = "127.0.0.1"
+PORT = 6700
 
-BUFSIZ = 1024
+BUFSIZ = 4096
 ADDR = (HOST, PORT)
 
 client_socket = socket(AF_INET, SOCK_STREAM)
--- chat server.py
+++ chat server.py
@@ -1,6 +1,7 @@
 """Server for multithreaded (asynchronous) chat application."""
 from socket import AF_INET, socket, SOCK_STREAM
 from threading import Thread
+import tkinter
 
 
 def handle_incoming_connections():
@@ -8,26 +9,72 @@ def handle_incoming_connections():
     while True:
         client, client_address = SERVER.accept()
         print("%s:%s has connected." % client_address)
+        welcomeMessage = "Connected to the server. Please type your name first."
+        client.send(bytes(welcomeMessage, "utf8"))
+        addresses[client] = client_address
         Thread(target=handle_client, args=(client,)).start()
 
 
-def handle_client(client):  # Takes client socket as argument.
+def handle_client(client):
     """Handles a single client connection."""
+
+    name = client.recv(bufSize).decode("utf8")
+    welcome = 'Welcome %s! If you ever want to quit, type {exit} to exit.' % name
+    client.send(bytes(welcome, "utf8"))
+    msg = "%s has joined the chat!" % name
+    broadcast(bytes(msg, "utf8"))
+    clients[client] = name
 
     while True:
         msg = client.recv(bufSize)
-        if msg != bytes("{quit}", "utf8"):
-            print("recieved : %s" % msg.decode("utf8"))
+        if msg != bytes("{exit}", "utf8"):
+            broadcast(msg, name + ": ")
         else:
-            client.send(bytes("{quit}", "utf8"))
+            client.send(bytes("{exit}", "utf8"))
             client.close()
-            print("client quit")
+            del clients[client]
+            broadcast(bytes("%s has left the chat." % name, "utf8"))
             break
 
 
+def broadcast(msg, prefix=""):
+    """Broadcasts a message to all the clients."""
+
+    for sock in clients:
+        sock.send(bytes(prefix, "utf8") + msg)
+
+
+def handle_incoming_client_message():
+    "Handle incoming client message"
+
+
+def display_clients():
+    "Show connected clients"
+    
+
+clients = {}
+addresses = {}
+
+top = tkinter.Tk()
+top.title("Server")
+
+messages_frame = tkinter.Frame(top)
+scrollbar = tkinter.Scrollbar(messages_frame)
+msg_list = tkinter.Listbox(messages_frame, height=15, width=50, yscrollcommand=scrollbar.set)
+scrollbar.pack(side=tkinter.RIGHT, fill=tkinter.Y)
+msg_list.pack(side=tkinter.LEFT, fill=tkinter.BOTH)
+messages_frame.pack()
+
+messages_frame = tkinter.Frame(top)
+scrollbar = tkinter.Scrollbar(messages_frame)
+msg_list = tkinter.Listbox(messages_frame, height=15, width=50, yscrollcommand=scrollbar.set)
+scrollbar.pack(side=tkinter.RIGHT, fill=tkinter.Y)
+msg_list.pack(side=tkinter.LEFT, fill=tkinter.BOTH)
+messages_frame.pack()
+
 host = '127.0.0.1'
 port = 6700
-bufSize = 1024
+bufSize = 4096
 address = (host, port)
 
 SERVER = socket(AF_INET, SOCK_STREAM)
@@ -38,5 +85,6 @@ print("Server start...")
 print("Waiting for connection...")
 ACCEPT_THREAD = Thread(target=handle_incoming_connections)
 ACCEPT_THREAD.start()
+tkinter.mainloop()
 ACCEPT_THREAD.join()
 SERVER.close()
